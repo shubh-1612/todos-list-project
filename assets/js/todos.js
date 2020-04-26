2a82721e147ac42595e671f3a16ec39ce28cff08
Shubh Tandon <shubhtandon7@gmail.com>
2020-04-26 17:12:16 +0530

    First commit

---
+++ assets/js/todos.js
@@ -0,0 +1,20 @@
+$("ul").on("click","li",function () {
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function (event) {
+	$(this).parent().fadeOut(500,function () {
+		$(this).remove();
+	});
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+	if (event.which===13){
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + $(this).val() + "</li>");
+		$(this).val("");}
+});
+
+$("#toggle-form").click(function () {
+	$("input[type='text']").fadeToggle();
+});
